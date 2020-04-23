3bb9df2e00a24fd17a1966942830024d28a2e420
Rishi J <flameclaw02@gmail.com>
2020-04-23 15:27:04 -0400

    MenuBaseWorkflow

---
+++ Assets/Scripts/SceneManage.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class SceneManage : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void onClick() {
+        if(SceneManager.GetActiveScene().buildIndex==0)
+            SceneManager.LoadScene(1);
+        if (SceneManager.GetActiveScene().buildIndex == 1)
+            SceneManager.LoadScene(0);
+    }
+}
